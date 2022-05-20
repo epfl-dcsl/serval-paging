b6d61c9451d378491b106c7c85ed9507fcecd2ff
Michael Paper <michael.paper@protonmail.com>
2022-05-20 09:02:35 +0200

    added sanity checks

--- include/paging.h
+++ include/paging.h
@@ -8,10 +8,8 @@
 
 /* 
  * Functions to map a frame to a given address
- * Each user is only given a single L4 entry, so `map_page_table_l3_entry`
- * does not need to take the L4 entry as an argument
  */
-int map_page_table_l3_entry(uint16_t l4_offset, pn_t l3e);
+int map_page_table_l3_entry(pn_t l4e, uint16_t l4_offset, pn_t l3e);
 int map_page_table_l2_entry(pn_t l3e, uint16_t l3_offset, pn_t l2e);
 int map_page_table_l1_entry(pn_t l2e, uint16_t l2_offset, pn_t l1e);
 int map_page_table_frame(pn_t l1e, uint16_t l1_offset, pn_t frame);
--- src/paging.c
+++ src/paging.c
@@ -22,12 +22,15 @@ void init_address_spaces(void)
 }
 
 
-int map_page_table_l3_entry(uint16_t l4_offset, pn_t l3e)
+int map_page_table_l3_entry(pn_t l4e, uint16_t l4_offset, pn_t l3e)
 {
   pn_t current_root = address_space_mapping[current];
 
   /* You can't map a PTE entry to an offset that doesn't fit on 9 bits */
   if (l4_offset >= 0x200)
+    return 1;
+  /* You can't map an entry that does not exist */
+  if (l4e > NUMBER_OF_FRAMES || l3e > NUMBER_OF_FRAMES)
     return 1;
   /* You can't map a PTE to your L4 root if it is not an L3 entry */
   if (frames_metadata[l3e].type != PAGE_L3_ENTRY)
@@ -39,6 +42,9 @@ int map_page_table_l3_entry(uint16_t l4_offset, pn_t l3e)
   /* You can't map to your address space a private PTE that you don't own */
   if (!(frames_metadata[l3e].permissions & PAGE_ACCESS_SHARED) &&
       frames_metadata[l3e].owner != current)
+    return 1;
+  /* You can't add mappings to an address space that is not yours */
+  if (frames_metadata[l4e].owner != current)
     return 1;
 
 
@@ -54,6 +60,9 @@ int map_page_table_l2_entry(pn_t l3e, uint16_t l3_offset, pn_t l2e)
   /* You can't map a PTE entry to an offset that doesn't fit on 9 bits */
   if (l3_offset >= 0x200)
     return 1;
+  /* You can't map an entry that does not exist */
+  if (l3e > NUMBER_OF_FRAMES || l2e > NUMBER_OF_FRAMES)
+    return 1;
   /* You can't map a PTE to an L3 PTE if it is not an L2 entry */
   if (frames_metadata[l3e].type != PAGE_L3_ENTRY)
     return 1;
@@ -66,6 +75,9 @@ int map_page_table_l2_entry(pn_t l3e, uint16_t l3_offset, pn_t l2e)
   /* You can't map to your address space a private PTE that you don't own */
   if (!(frames_metadata[l2e].permissions & PAGE_ACCESS_SHARED) &&
       frames_metadata[l2e].owner != current)
+    return 1;
+  /* You can't add mappings to an address space that is not yours */
+  if (frames_metadata[l3e].owner != current)
     return 1;
 
   ((void**) (all_frames[l3e].content)) [l3_offset] = &all_frames[l2e];
@@ -81,6 +93,9 @@ int map_page_table_l1_entry(pn_t l2e, uint16_t l2_offset, pn_t l1e)
   /* You can't map a PTE entry to an offset that doesn't fit on 9 bits */
   if (l2_offset >= 0x200)
     return 1;
+  /* You can't map an entry that does not exist */
+  if (l2e > NUMBER_OF_FRAMES || l1e > NUMBER_OF_FRAMES)
+    return 1;
   /* You can't map a PTE to an L2 PTE if it is not an L1 entry */
   if (frames_metadata[l2e].type != PAGE_L2_ENTRY)
     return 1;
@@ -93,6 +108,9 @@ int map_page_table_l1_entry(pn_t l2e, uint16_t l2_offset, pn_t l1e)
   /* You can't map to your address space a private PTE that you don't own */
   if (!(frames_metadata[l1e].permissions & PAGE_ACCESS_SHARED) &&
       frames_metadata[l1e].owner != current)
+    return 1;
+  /* You can't add mappings to an address space that is not yours */
+  if (frames_metadata[l2e].owner != current)
     return 1;
 
   ((void**) (all_frames[l2e].content)) [l2_offset] = &all_frames[l1e];
@@ -107,6 +125,9 @@ int map_page_table_frame(pn_t l1e, uint16_t l1_offset, pn_t frame)
   /* You can't map a frame to an offset that doesn't fit on 9 bits */
   if (l1_offset >= 0x200)
     return 1;
+  /* You can't map a frame or an entry that does not exist */
+  if (l1e > NUMBER_OF_FRAMES || frame > NUMBER_OF_FRAMES)
+    return 1;
   /* You can't map a PTE to an L1 PTE if it is not a frame */
   if (frames_metadata[l1e].type != PAGE_L1_ENTRY)
     return 1;
@@ -119,6 +140,9 @@ int map_page_table_frame(pn_t l1e, uint16_t l1_offset, pn_t frame)
   /* You can't map to your address space a private frame that you don't own */
   if (!(frames_metadata[frame].permissions & PAGE_ACCESS_SHARED) &&
       frames_metadata[frame].owner != current)
+    return 1;
+  /* You can't add mappings to an address space that is not yours */
+  if (frames_metadata[l1e].owner != current)
     return 1;
 
   ((void**) (all_frames[l1e].content)) [l1_offset] = &all_frames[frame];
@@ -133,6 +157,8 @@ int unmap_page_table_entry(pn_t pte, uint16_t offset, pn_t pte_entry)
   /* You can't unmap an offset that is not addressable with 9 bits */
   if (offset >= 0x200)
     return 1;
+  if (pte > NUMBER_OF_FRAMES)
+    return 1;
   /* You can't unmap an entry of something other than a PTE */
   if (frames_metadata[pte].type != PAGE_L4_ENTRY &&
       frames_metadata[pte].type != PAGE_L3_ENTRY &&
@@ -141,6 +167,9 @@ int unmap_page_table_entry(pn_t pte, uint16_t offset, pn_t pte_entry)
     return 1;
   /* You can't unmap an empty entry */
   if (((void**) (&all_frames[pte])) [offset] == NULL)
+    return 1;
+  /* You can't unmap a page that does not belong to you */
+  if (frames_metadata[pte].owner != current)
     return 1;
 
   ((void**) (all_frames[pte].content)) [offset] = NULL;
